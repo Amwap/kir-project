aeda79afb6544ab9f1425ddd0bff67dfed84079c
Amwap <38501564+Amwap@users.noreply.github.com>
2021-05-29 21:41:46 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,6 @@
+timeout 3
+start python "C:\Release\main.py" --user "nhuj997" --pw "2211334455!"
+timeout 3
+start python "C:\Release\main.py" --user "nhuj997" --pw "2211334455!"
+timeout 3
+start python "C:\Release\main.py" --user "nhuj997" --pw "2211334455!"
